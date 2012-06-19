d0955d1b45e70c81da8087eec7d05463877c4255
Tasos Latsas <tlatsas2000@gmail.com>
2012-06-19 20:57:14 +0300

    skeleton code for use with gps

---
+++ src/com/incidentlocator/client/GetLocationListener.java
@@ -0,0 +1,25 @@
+package com.incidentlocator.client;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import android.os.Bundle;
+
+public class GetLocationListener implements LocationListener {
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        // do something
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+}
--- src/com/incidentlocator/client/IncidentLocator.java
+++ src/com/incidentlocator/client/IncidentLocator.java
@@ -3,20 +3,39 @@ package com.incidentlocator.client;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
+//import android.content.Intent;
+
+import android.widget.TextView;
+import java.lang.String;
+
+import android.content.Context;
+import android.location.LocationManager;
+import com.incidentlocator.client.GetLocationListener;
 
 public class IncidentLocator extends Activity
 {
+
+    private LocationManager locationManager;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        locationManager =
+            (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // TODO: request both gps and network updates
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER, 0, 0, new GetLocationListener());
     }
 
     public void getLocation(View view) {
-        Intent intent = new Intent(this, GetLocationActivity.class);
-        startActivity(intent);
+        String message = "Hello from incident Locator";
+
+        TextView textView = (TextView) findViewById(R.id.show_message);
+        textView.setText(message);
     }
 }
