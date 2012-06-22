6a293164df50cb7924e51baa84e42be999334d99
Tasos Latsas <tlatsas2000@gmail.com>
2012-06-22 20:45:07 +0300

    prompt user to enable gps if disabled

--- src/com/incidentlocator/client/IncidentLocator.java
+++ src/com/incidentlocator/client/IncidentLocator.java
@@ -1,15 +1,20 @@
 package com.incidentlocator.client;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.DialogInterface;
+
 import android.location.LocationManager;
 import android.location.Location;
 import android.location.LocationListener;
 
+import android.provider.Settings;
 import android.util.Log;
 
 
@@ -39,6 +44,28 @@ public class IncidentLocator extends Activity
     public void onStart() {
         super.onStart();
 
+        // check if GPS is enabled
+        boolean isGpsEnabled =
+            locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (! isGpsEnabled) {
+            // create a new dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder
+                .setTitle("GPS is disabled")
+                .setMessage("Do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // intent to open settings
+                        Intent settings = new Intent(
+                            Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(settings);
+                    }
+                });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
         // TODO: request both gps and network updates
         locationManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER, 0, 0, new GetLocationListener());
