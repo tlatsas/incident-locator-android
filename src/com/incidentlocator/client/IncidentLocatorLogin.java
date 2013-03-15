159db1de79b6392e0cbe7e5bdd0a47dcd88bb028
Tasos Latsas <tlatsas2000@gmail.com>
2013-03-15 16:46:41 +0200

    remember login data

--- src/com/incidentlocator/client/IncidentLocatorLogin.java
+++ src/com/incidentlocator/client/IncidentLocatorLogin.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.util.Log;
+import android.webkit.URLUtil;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -14,12 +17,19 @@ import com.incidentlocator.client.HttpRest;
 
 public class IncidentLocatorLogin extends Activity {
     private static final String TAG = "IncidentLocatorLogin";
+    private static final String PREFS = "IncidentLocatorPreferences";
     private static Context context;
+    private SharedPreferences settings;
 
     private EditText usernameTxt;
     private EditText passwordTxt;
+    private EditText hostTxt;
 
     private HttpRest http = new HttpRest(IncidentLocatorLogin.this);
+
+    private String username;
+    private String password;
+    private String host;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,16 +41,53 @@ public class IncidentLocatorLogin extends Activity {
 
         usernameTxt = (EditText)findViewById(R.id.txt_username);
         passwordTxt = (EditText)findViewById(R.id.txt_password);
+        hostTxt = (EditText)findViewById(R.id.txt_host);
+
+        // retrieve last used credentials from settings
+        settings = context.getSharedPreferences(PREFS, 0);
+        username = settings.getString("username", "");
+        password = settings.getString("password", "");
+        host = settings.getString("host", "http://10.0.2.2:3000");
+
+        usernameTxt.setText(username);
+        passwordTxt.setText(password);
+        hostTxt.setText(host);
     }
 
     public void httpLogin(View view) {
         Map<String, String> data = new HashMap<String, String>();
-        data.put("email", usernameTxt.getText().toString());
-        data.put("password", passwordTxt.getText().toString());
 
-        // TODO: get this from an optional text field
-        http.setHost("http://10.0.2.2:3000/");
+        username = usernameTxt.getText().toString().trim();
+        password = passwordTxt.getText().toString().trim();
+        host = hostTxt.getText().toString().trim();
 
-        http.login(data);
+        if (username.equals("") || password.equals("")) {
+            Toast toast = Toast.makeText(context,
+                "Email and password are required", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+
+        if (URLUtil.isHttpUrl(host) || URLUtil.isHttpsUrl(host)) {
+            // on valid data save on settings and call login task
+            settings = context.getSharedPreferences(PREFS, 0);
+            SharedPreferences.Editor editor = settings.edit();
+
+            editor.putString("username", username);
+            editor.putString("password", password);
+            editor.putString("host", host);
+            editor.commit();
+
+            http.setHost(host);
+
+            // set data for login
+            data.put("email", username);
+            data.put("password", password);
+            http.login(data);
+        } else {
+            Toast toast = Toast.makeText(context,
+                "Invalid host URL", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
